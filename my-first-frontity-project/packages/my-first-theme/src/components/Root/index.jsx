f819dd9516d5ccddb83908e9839fbd76c49bf0e4
Saulo <saulo.monteiro.dev@gmail.com>
2021-09-12 10:10:22 -0300

    add react-bootstrap

--- my-first-frontity-project/packages/my-first-theme/src/components/Root/index.jsx
+++ my-first-frontity-project/packages/my-first-theme/src/components/Root/index.jsx
@@ -1,35 +1,42 @@
-import { connect } from 'frontity'
-import Link from '@frontity/components/link'
-import Switch from '@frontity/components/switch'
-import List from '../List';
+import { Global, css, connect } from "frontity";
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import List from "../List";
 import Post from "../Post";
-import Page from '../Page';
+import Page from "../Page";
+import { fixCss } from "../../helpers/css";
+
+import bootstrapCss from "bootstrap/dist/css/bootstrap.min.css";
+
+const fixedBootstrapCss = fixCss(bootstrapCss);
+const BootstrapStyles = () => <Global styles={css(fixedBootstrapCss)} />;
 
 const Root = ({ state }) => {
-    const data = state.source.get(state.router.link)
+  const data = state.source.get(state.router.link);
 
-    return (
-      <>
-        <p> Current URL: {state.router.link}</p>
-        <nav>
-          <Link link="/">Home</Link>
-          <br />
-          <Link link="/page/2">More posts</Link>
-          <br />
-          <Link link="/about-us">About Us</Link>
-        </nav>
-        <div>
-          <main>
-            <Switch>
-              <List when={data.isArchive} />
-              <div when={data.isArchive}>This is a list</div>
-              <Post when={data.isPost}>This is a post</Post>
-              <Page when={data.isPage}>This is a page</Page>
-            </Switch>
-          </main>
-        </div>
-      </>
-    );
-}
+  return (
+    <>
+      <BootstrapStyles />
+      <p> Current URL: {state.router.link}</p>
+      <nav>
+        <Link link="/">Home</Link>
+        <br />
+        <Link link="/page/2">More posts</Link>
+        <br />
+        <Link link="/about-us">About Us</Link>
+      </nav>
+      <div>
+        <main>
+          <Switch>
+            <List when={data.isArchive} />
+            <div when={data.isArchive}>This is a list</div>
+            <Post when={data.isPost}>This is a post</Post>
+            <Page when={data.isPage}>This is a page</Page>
+          </Switch>
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default connect(Root)
+export default connect(Root);
